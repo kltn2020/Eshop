3d124420c923e4dc9a3a81a48e95f8c27fb2c526
JHVy <16521470@gm.uit.edu.vn>
2020-10-03 22:32:34 +0700

    add jenkins

---
+++ jenkins/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker rm -f eshop-backend
+
+docker rmi $(docker images -qa -f 'dangling=true')
+
+docker run -d --name eshop-backend \
+  --network my-net \
+  -e DATABASE_URL=postgres://postgres:postgres@localhost:5432/eshop \
+  -e SECRET_KEY_BASE=43HToczwEtbLIujVyC/L7syuUDAEHklOVydWoxt6Tu33IfUGT8ZaQbpVTcH5jnw8 \
+  -e SECRET_JOKEN=43HToczwEtbLIujVyC/L7syuUDAEHklOVydWoxt6Tu33IfUGT8ZaQbpVTcH5jnw8 \
+  -e CLOUDEX_API_KEY=141644434633821 \
+  -e CLOUDEX_SECRET=02TvjPRBlZa7rnhp8MOUy_HVoWM \
+  -e CLOUDEX_CLOUD_NAME=dudnown4z \
+  -e CLOUDEX_CLOUD_ENV=test \
+  -e SENTRY_DSN=https://de992d97744c4527a8ba64ce3d7141e9:688b0a2f41c14a6a97b533d755b71dce@o437505.ingest.sentry.io/5400205 \
+  -p 4000:4000 \
+  ${DOCKER_IMAGE}:$BUILD_NUMBER
