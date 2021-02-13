02ccd6efe99734131b40d825461417088f68994a
JHVy <16521470@gm.uit.edu.vn>
2021-02-13 17:36:05 +0700

    update config

--- apps/eshop_client/config/prod.exs
+++ apps/eshop_client/config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :eshop_client, EshopClientWeb.Endpoint,
   url: [host: "example.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
