f7233f989b2d70777aebe4bac806195683bf2f2b
JHVy <16521470@gm.uit.edu.vn>
2021-02-13 13:46:50 +0700

    clean code client

--- apps/eshop_client/lib/eshop_client_web/controllers/rating/review_controller.ex
+++ apps/eshop_client/lib/eshop_client_web/controllers/rating/review_controller.ex
@@ -2,7 +2,6 @@ defmodule EshopClientWeb.Rating.ReviewController do
   use EshopClientWeb, :controller
 
   alias EshopCore.Rating
-  alias EshopCore.Rating.Review
 
   action_fallback EshopClientWeb.FallbackController
 
@@ -13,37 +12,6 @@ defmodule EshopClientWeb.Rating.ReviewController do
       paging.entries
       |> EshopCore.Repo.preload(:user)
       |> EshopCore.Repo.preload(replies: :user)
-      |> EshopCore.Utils.StructHelper.to_map()
-      |> Enum.map(fn review ->
-        Map.put(
-          review,
-          :user,
-          Map.take(review.user, [:email, :id])
-        )
-      end)
-      |> Enum.map(fn review ->
-        replies =
-          review.replies
-          |> Enum.map(fn reply ->
-            Map.put(
-              reply,
-              :user,
-              Map.take(reply.user, [:email, :id])
-            )
-          end)
-
-        Map.put(review, :replies, replies)
-      end)
-
-    conn |> json(%{status: "OK", data: %{paging | entries: entries}})
-  end
-
-  def admin_index(conn, params) do
-    paging = Rating.admin_list_reviews_with_paging(params)
-
-    entries =
-      paging.entries
-      |> EshopCore.Repo.preload([:user, [replies: :user]])
       |> EshopCore.Utils.StructHelper.to_map()
       |> Enum.map(fn review ->
         Map.put(
@@ -91,17 +59,6 @@ defmodule EshopClientWeb.Rating.ReviewController do
             |> EshopClientWeb.ChangesetView.translate_errors()
             |> EshopCore.Utils.Validator.get_validation_error_message()
         })
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    review = Rating.get_review!(id)
-
-    product_id = review.product_id
-
-    with {:ok, %Review{}} <- Rating.delete_review(review) do
-      Rating.update_rating(product_id)
-      conn |> json(%{status: "OK"})
     end
   end
 end
