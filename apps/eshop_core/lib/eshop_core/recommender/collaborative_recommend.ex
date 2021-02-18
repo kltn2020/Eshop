4d095a154723964d94ea65808840559677cdc760
JHVy <16521470@gm.uit.edu.vn>
2021-02-18 16:27:34 +0700

    update logger tesla

--- apps/eshop_core/lib/eshop_core/recommender/collaborative_recommend.ex
+++ apps/eshop_core/lib/eshop_core/recommender/collaborative_recommend.ex
@@ -1,18 +1,22 @@
 defmodule EshopCore.Recommender.CollaborativeRecommend do
-  use Tesla
-
-  plug Tesla.Middleware.BaseUrl, System.get_env("RECOMMENDER_HOST")
-  plug Tesla.Middleware.JSON
-  plug Tesla.Middleware.Logger
-
   def perform(user_id, limit) do
-    with {:ok, res} <- fetch_product_ids(user_id, limit),
-         ids <- res.body do
-      {:ok, ids}
+    with {:ok, %{body: body}} <- fetch_product_ids(user_id, limit) do
+      {:ok, body}
     end
   end
 
   def fetch_product_ids(user_id, limit) do
-    get("/collaborative_recommend?user_id=#{user_id}&limit=#{limit}")
+    api_client()
+    |> Tesla.get("/collaborative_recommend?user_id=#{user_id}&limit=#{limit}")
+  end
+
+  defp api_client() do
+    middleware = [
+      {Tesla.Middleware.BaseUrl, System.get_env("RECOMMENDER_HOST")},
+      Tesla.Middleware.JSON,
+      Tesla.Middleware.Logger
+    ]
+
+    Tesla.client(middleware)
   end
 end
--- apps/eshop_core/lib/eshop_core/recommender/content_based_recommend.ex
+++ apps/eshop_core/lib/eshop_core/recommender/content_based_recommend.ex
@@ -1,18 +1,24 @@
 defmodule EshopCore.Recommender.ContentBasedRecommend do
-  use Tesla
-
-  plug Tesla.Middleware.BaseUrl, System.get_env("RECOMMENDER_HOST")
-  plug Tesla.Middleware.JSON
-  plug Tesla.Middleware.Logger
-
   def perform(user_id, product_id, limit) do
-    with {:ok, res} <- fetch_product_ids(user_id, product_id, limit),
-         ids <- res.body do
-      {:ok, ids}
+    with {:ok, %{body: body}} <- fetch_product_ids(user_id, product_id, limit) do
+      {:ok, body}
     end
   end
 
   def fetch_product_ids(user_id, product_id, limit) do
-    get("/content_based_recommend?user_id=#{user_id}&product_id=#{product_id}&limit=#{limit}")
+    api_client()
+    |> Tesla.get(
+      "/content_based_recommend?user_id=#{user_id}&product_id=#{product_id}&limit=#{limit}"
+    )
+  end
+
+  defp api_client() do
+    middleware = [
+      {Tesla.Middleware.BaseUrl, System.get_env("RECOMMENDER_HOST")},
+      Tesla.Middleware.JSON,
+      Tesla.Middleware.Logger
+    ]
+
+    Tesla.client(middleware)
   end
 end
